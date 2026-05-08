6dda48cd24f856478a2b136d58d652a6d4f7e423
uid25976 <prochaska.arnaud@orange.fr>
2026-05-08 20:42:06 +0200

    baseline from ThinkPad

---
+++ CO2_SGP30_M5stick.ino
@@ -0,0 +1,223 @@
+/**
+ * CO2 sensor using a M5stick plus and SGP30 sensor
+ * Board library : ESP32 3.2.0
+ * Libraries: Adafruit SGP30 2.0.3 - M5Family 0.1.3 - M5GFX 0.2.9 - M5HAL 0.0.1 - M5Stack 0.4.6 - M5StickCPlus2 1.0.1 - M5Unified 0.2.7 - M5Utility 0.0.9
+ * Original SGP30 library from Adafruit caused conflicts with the ESP32 SPI driver: we override
+ * it to change the SPI driver that it used: this required to change from "private" to "protected" some members of original Adafruit class
+ * History:
+ * 2025-12-27: v 0.9 first version
+ *
+ * @TODO add buzzer
+ * @TODO move UI to a separate class
+ * @TODO add detection of stable MAX
+ * @TODO: add MAX detection + reset using button
+ * @TODO: add low power
+ *
+ */
+#include <M5Unified.h>
+#include <Wire.h>
+#include "SGP30_M5.h"
+
+// proto ----------------------------------------------------
+void displayMainValue(float fval);
+void displayMAXvalue(float fval);
+
+
+// customize ------------------------------------------------
+#define DUMP_ON_SERIAL
+
+// constant --------------------------------------------------
+// I2C pins of the M5 grove connector
+#define GPIO_SDA_EXT 32
+#define GPIO_SCL_EXT 33
+
+// tune humidity compensation: page 10 of data sheet
+// target: 13degC 95%HR (optimized for caves)
+// units : mg/m3
+const uint32_t HR_MG_M3 = 10753;
+// sensor delay before operational
+const uint32_t stabilization_time_ms = 15000;
+// CO2 thresholds to notify level of danger 
+const float thres1 = 0.1;
+const float thres2 = 0.5;
+const float thres3 = 4;
+const int header_height = 35;
+const int footer_height = 45;
+
+// variables ------------------------------------------------
+// sensor object
+SGP30_M5 sgp;
+static uint32_t last_millis = 0;
+static int16_t lcd_width = 0;
+static int16_t lcd_height = 0;
+static float co2percent = 0;
+static float co2percentMAX = 0;
+
+static int main_height;
+
+M5Canvas canvasHeader(&M5.Lcd);
+M5Canvas canvasMain(&M5.Lcd);
+M5Canvas canvasFooter(&M5.Lcd);
+
+
+// ======================== INIT =============================
+// ===========================================================
+void setup() 
+{
+	auto cfg = M5.config();  // Assign a structure for initializing M5Stack
+    
+    M5.begin(cfg);  // initialize M5 device
+    M5.Lcd.clear(TFT_BLACK);
+    lcd_width = M5.Lcd.width();
+    lcd_height = M5.Lcd.height();
+
+    main_height = lcd_height-(header_height + footer_height);
+    
+    
+    
+    // I2C init -------------------	
+    Wire.begin(GPIO_SDA_EXT, GPIO_SCL_EXT); // Initialize I2C with SDA pin 32 and SCL pin 33 for M5StickC Plus2
+    Wire.setClock(400000); // Set I2C clock speed to 400 kHz
+	
+	// SW reset using general call addr --------    
+    sgp.softReset();
+	
+	// Serial port init for debug --------------
+	Serial.begin(115200);
+
+
+    // Init the sensor -------------------------
+	bool report = sgp.begin();	
+	if (!report) 
+    {
+      Serial.println("INIT FAILED");
+	  while (1);
+    } 
+
+    // create header ------------------------------
+    canvasHeader.createSprite(lcd_width, header_height);
+    canvasHeader.fillSprite(TFT_NAVY);    
+    canvasHeader.setTextSize(3);
+    canvasHeader.setTextColor(TFT_YELLOW);  
+    canvasHeader.drawString("WAIT",lcd_width/2-int(lcd_width/5), 5);
+    canvasHeader.pushSprite(&M5.Lcd, 0, 0);//"&M5
+	
+
+    // create footer ------------------------------
+    canvasFooter.createSprite(lcd_width, footer_height);
+    canvasFooter.fillSprite(TFT_DARKGREY);    
+    canvasFooter.setTextSize(4);
+    canvasFooter.setTextColor(TFT_BLACK);  
+    canvasFooter.drawString("MAX: ",1, 5);
+    canvasFooter.pushSprite(&M5.Lcd, 0, lcd_height-footer_height);
+
+    // create main ------------------------------
+    canvasMain.createSprite(lcd_width, main_height);
+    canvasMain.fillSprite(TFT_BLACK);    
+    canvasMain.setTextSize(4);
+    canvasMain.setTextColor(TFT_WHITE);  
+    canvasMain.drawString("...",lcd_width/3, int(main_height/5));
+    canvasMain.pushSprite(&M5.Lcd, 0, header_height);
+
+
+    // set humidity ------------------------------
+    report = sgp.setHumidity(HR_MG_M3);
+    if (!report) 
+    {
+      Serial.println("HR FIX FAILED");
+	  while (1);
+    } 
+
+    // dummy measurements during stabilization time
+	uint32_t init_start_ts = millis();;
+    while ((millis() - init_start_ts) < stabilization_time_ms)
+	{		
+		report = sgp.IAQmeasure();
+        delay(1000);
+	}
+
+
+	
+	if (report)
+	{
+		canvasHeader.fillSprite(TFT_NAVY);    
+        canvasHeader.drawString("%CO2",lcd_width/2-int(lcd_width/5), 5);
+        canvasHeader.pushSprite(&M5.Lcd, 0, 0);
+	} else
+	{
+		Serial.println("INIT FAILED");
+	}	
+}
+
+
+
+
+// ======================== LOOP =============================
+// ===========================================================
+void loop() 
+{
+   if (!sgp.IAQmeasure()) 
+   {  // Commands the sensor to take a single eCO2/VOC                              
+      Serial.println("Measurement failed");      
+   } else
+   {
+        co2percent = (float)sgp.eCO2/10000; // 100(convert to %)/1000000(ppm) =  10000
+	        
+        displayMainValue(co2percent);
+
+#ifdef DUMP_ON_SERIAL
+        Serial.printf("\nCO2 %d ppm", sgp.eCO2);
+        Serial.printf("\nCO2 %.2f %%", co2percent);
+#endif
+        if (co2percent > co2percentMAX)
+        {
+            co2percentMAX = co2percent;
+            displayMAXvalue(co2percentMAX);
+        }
+   }
+
+   // one sec delay required between measurements
+   delay(1000);
+}
+
+
+
+
+void displayMainValue(float fval)
+{
+    // update main panel ---------------------
+    canvasMain.clear(TFT_BLACK);
+
+    // customize color according to the risk 
+    if (fval < thres1)
+    {
+        canvasMain.setTextColor(TFT_GREEN);
+    } else if (fval < thres2)
+    {
+       canvasMain.setTextColor(TFT_ORANGE);
+    } else if (fval < thres3)
+    {
+        canvasMain.setTextColor(TFT_PURPLE);
+    } else 
+    {
+        canvasMain.setTextColor(TFT_RED);
+    }
+    // value, nb decimals, X offset, Y offset
+    canvasMain.drawFloat(fval, 1, lcd_width/4, int(main_height/5));
+    canvasMain.pushSprite(&M5.Lcd, 0, header_height);
+}
+
+
+void displayMAXvalue(float fval)
+{    
+    // update footer panel ---------------------      
+    canvasFooter.setTextSize(4);
+    canvasFooter.setTextColor(TFT_BLACK);  
+    canvasFooter.clear(TFT_DARKGREY);
+
+    //canvasFooter.drawString("MAX: ",1, 5);
+    // value, nb decimals, X offset, Y offset
+    canvasFooter.drawFloat(fval, 1, int(lcd_width/3), 5);
+    canvasFooter.pushSprite(&M5.Lcd, 0, (lcd_height-footer_height) );
+}
+
---
+++ SGP30_M5.cpp
@@ -0,0 +1,209 @@
+/*!
+ * @file Adafruit_SGP30.cpp
+ *
+ * @mainpage Adafruit SGP30 gas sensor driver
+ *
+ * @section intro_sec Introduction
+ *
+ * This is the documentation for Adafruit's SGP30 driver for the
+ * Arduino platform.  It is designed specifically to work with the
+ * Adafruit SGP30 breakout: http://www.adafruit.com/products/3709
+ *
+ * These sensors use I2C to communicate, 2 pins (SCL+SDA) are required
+ * to interface with the breakout.
+ *
+ * Adafruit invests time and resources providing this open source code,
+ * please support Adafruit and open-source hardware by purchasing
+ * products from Adafruit!
+ *
+ *
+ * @section author Author
+ * Written by Ladyada for Adafruit Industries.
+ *
+ * @section license License
+ * BSD license, all text here must be included in any redistribution.
+ *
+ */
+#include "Arduino.h"
+#include "SGP30_M5.h"
+
+
+
+/*!
+ *  @brief  Instantiates a new SGP30 class
+ */
+SGP30_M5::SGP30_M5() {}
+
+
+
+/*!
+ *  @brief  Setups the hardware and detects a valid SGP30. Initializes I2C
+ *          then reads the serialnumber and checks that we are talking to an SGP30
+ 
+ *  @param  initSensor
+ *          Optional pointer to prevent IAQinit to be called. Used for Deep
+ *          Sleep.
+ *  @return True if SGP30 found on I2C, False if something went wrong!
+ */
+boolean SGP30_M5::begin(boolean initSensor)
+ {  
+  uint8_t command[2];
+  
+  // get serial number to check wether the sensor is connected to the bus
+  command[0] = 0x36;
+  command[1] = 0x82;
+  if (!readWordFromCommand(command, 2, 10, serialnumber, 3))
+    return false;
+
+  // check the sensor version for code compatibility --------------------
+  uint16_t featureset;
+  command[0] = 0x20;
+  command[1] = 0x2F;
+  if (!readWordFromCommand(command, 2, 10, &featureset, 1))
+    return false;
+
+  if ((featureset & 0xF0) != SGP30_FEATURESET)
+    return false;
+
+
+  // init sensor --------------------------------------------------------
+  if (initSensor) 
+  {
+    if (!IAQinit())
+      return false;
+  }
+
+  CO2max = 0;
+
+   return true;
+}
+
+
+bool SGP30_M5::measureCO2()
+{
+    bool report = IAQmeasure();
+
+    if (true == report)
+    {
+        CO2percent = (float)this->eCO2/10000;
+
+        if (CO2percent > CO2max)
+        {
+            CO2max = CO2percent;
+        }
+
+    }
+
+    return report;
+}
+
+
+float SGP30_M5::getterCO2percent()
+{
+  return CO2percent;
+}
+
+float SGP30_M5::getterCO2filtered()
+{
+  return CO2filtered;
+}
+
+float SGP30_M5::getterCO2max()
+{
+  return CO2max;
+}
+
+
+// ======================================================================
+// Sending 06h at address 00h: means general reset to all slave devices
+bool SGP30_M5::softReset()
+{
+	uint8_t command = 0x06;
+	Wire.beginTransmission(0x00);
+	bool report = Wire.write(&command, 1);     
+	Wire.endTransmission(true);
+  return report;
+}
+
+
+
+/**
+     * @brief Send a command to the slave device (device_addr) and eventually
+     * reads one or more words
+     * @param command pointer to the array of bytes that contains the command.
+     * @param commandLength is the number of bytes in the command
+     * @param delay_ms is a pause after the transmission of the command
+     * @param readdata is a pointer of an array of words that will receive the slave answer (skip if no answer expected)
+     * @param readlen is the number of words expected fro; the slave (skip if no answer expected)
+     * @return false if something went wrong 
+     * @remark This API is bespoke for the SGP30 sensor where each word that is returned is appended with a CRC8
+     * The CRC will be checked but not copied to the readdata buffer
+     */
+bool SGP30_M5::readWordFromCommand(uint8_t command[], uint8_t commandLength, uint16_t delay_ms, uint16_t *readdata, uint8_t readlen) 
+{
+  // send start bit + addr ---------------------------------------
+  Wire.beginTransmission(this->device_addr);
+  
+  // send command ------------------------------------------------
+  size_t nb_wr = Wire.write(command, commandLength); 
+  if (nb_wr != commandLength)
+  {
+    Serial.printf("\nEBUG-2C: write returns %d", nb_wr);
+     return false;
+  }
+
+  // end of communication: set wr/rd bit and send a stop
+  Wire.endTransmission(true);
+  
+  // let slave enough time to prepare the answer -----------------
+  delay(delay_ms);
+
+  // is there some data to read ----------------------------------
+  if (readlen != 0)
+  {
+      // some data to read 
+      // In this component one CRC is sent for each word
+      uint8_t replylen = readlen * (WORD_LEN + 1);
+      uint8_t replybuffer[replylen];
+      
+      // read bytes and send a stop
+      size_t nb_rx = Wire.requestFrom(device_addr, replylen, true);
+      if (nb_rx != replylen) 
+      {
+        Serial.printf("\nDEBUG-2C: requestFrom returns %d", nb_rx);
+        return false;
+      }
+
+      // read bytes -----------------------------
+      for (int idx = 0; idx < replylen; idx++)    
+      { 
+        replybuffer[idx] = Wire.read(); // receive a byte as character
+      }
+
+      // check CRC and copy to remote buffer ----
+      for (uint8_t i = 0; i < readlen; i++) 
+      {
+          uint8_t crc = this->generateCRC(replybuffer + i * 3, 2);
+
+          if (crc != replybuffer[i * 3 + 2])
+          {
+            Serial.printf("\nDEBUG-2C: CRC failed on %d bytes\n", replylen);
+            for (int i=0; i< replylen;i++)
+            {
+                Serial.printf("%02X ", replybuffer[i]);
+            }
+            
+            return false;
+          }
+          
+          // else success store it without CRC
+          readdata[i] = replybuffer[i * 3];
+          readdata[i] <<= 8;
+          readdata[i] |= replybuffer[i * 3 + 1];
+      } // end loop on each word
+
+  } // end of slave response processing
+
+  return true;
+}
+
